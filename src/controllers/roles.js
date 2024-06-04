53db1e9e43d55c035222bb0803ed7f71549b3894
Gabriel Zepeda <gabrielzepeda846@gmail.com>
2024-06-03 18:44:15 -0600

    Added new endpoint to create new roles.

--- src/controllers/roles.js
+++ src/controllers/roles.js
@@ -1,12 +1,27 @@
 const { users } = require("./users");
 
 const getRoles = (req, res) => {
-  const roles = users.map(user => user.role);
-  res.json({ roles })
-}
+  const roles = users.map((user) => user.role);
+  res.json({ roles });
+};
 
 // TODO: Create a new Role
+const createRole = (req, res) => {
+  const { role } = req.body;
+
+  if (users.some((user) => user.role === role)) {
+    //role already exists
+    return res.status(400).json({ message: "This role already exists!" });
+  }
+
+  users.push({ role });
+
+  return res
+    .status(201)
+    .json({ message: `New role ${role} has been created successfully` });
+};
 
 module.exports = {
-  getRoles
-}
+  getRoles,
+  createRole,
+};
